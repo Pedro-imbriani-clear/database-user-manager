6b0be0b166df608fe44c13fd8ffe0a2f55a034f2
Pedro Imbriani <pedro.imbriani.clear@gmail.com>
2023-02-28 13:07:09 -0300

    configuring put, post, delete method

--- client-server/routes/users.js
+++ client-server/routes/users.js
@@ -1,20 +1,64 @@
 var express = require('express');
+var assert = require('assert');
 var restify = require('restify-clients');
 var router = express.Router();
-var assert = require('assert');
 
 var client = restify.createJsonClient({
-  url: 'https://localhost:3000'
+    url: 'http://localhost:4000'
+})
+
+
+router.get('/', function(req, res, next) {
+
+    client.get('/users', function(err, request, response, obj) {
+        assert.ifError(err);
+
+        res.json(obj);
+    });
+
 });
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  client.get('/users', function(err, request, response, obj) {
-    assert.ifError(err);
-  
-    res.end(JSON.stringify(obj, null, 2));
-  });
-  
+router.get('/:id', function(req, res, next) {
+
+    client.get(`/users/${req.params.id}`, function(err, request, response, obj) {
+        assert.ifError(err);
+
+        res.json(obj);
+    });
+
+});
+
+router.put('/:id', function(req, res, next) {
+
+    client.put(`/users/${req.params.id}`, req.body, function(err, request, response, obj) {
+        assert.ifError(err);
+
+        res.json(obj);
+
+    });
+
+});
+
+router.delete('/:id', function(req, res, next) {
+
+    client.del(`/users/${req.params.id}`, function(err, request, response, obj) {
+        assert.ifError(err);
+
+        res.json(obj);
+
+    });
+
+});
+
+router.post('/', function(req, res, next) {
+
+    client.post(`/users`, req.body, function(err, request, response, obj) {
+        assert.ifError(err);
+
+        res.json(obj);
+
+    });
+
 });
 
 module.exports = router;
